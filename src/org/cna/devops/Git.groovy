3e7cce2cda72d1a7ad716743bdf650251f668cee
aldo2510 <josealdotrucios@gmail.com>
2025-09-07 23:09:59 -0500

    Update Git.groovy

--- src/org/cna/devops/Git.groovy
+++ src/org/cna/devops/Git.groovy
@@ -1,18 +1,14 @@
-package org.cna.devops
+package org.acme.devops
 
 class Git implements Serializable {
-
   static String branch(script) {
     if (script.env.BRANCH_NAME) return script.env.BRANCH_NAME
+    if (script.env.GIT_BRANCH)  return script.env.GIT_BRANCH
     return script.sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
   }
-
   static String commit(script) {
     if (script.env.GIT_COMMIT) return script.env.GIT_COMMIT
     return script.sh(script: "git rev-parse HEAD", returnStdout: true).trim()
   }
-
-  static String shortSha(String full) {
-    return full?.take(8)
-  }
+  static String shortSha(String full) { full?.take(8) }
 }
